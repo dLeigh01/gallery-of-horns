2798ad53b928f540e5b6c486f38dc44f75e9e9d5
Dana Huffman <dana.huffman@yahoo.com>
2022-05-12 15:03:33 -0700

    Completed Feature 1

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import Filter from '../Filter/Filter.js'
 
 class Header extends React.Component {
   render() {
     return(
       <header>
         <h1>Gallery of Horns</h1>
+        <Filter handleSelect={this.props.handleSelect}></Filter>
       </header>
     );
   }
